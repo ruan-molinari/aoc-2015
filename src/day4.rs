17fac11ef3a225a69cbd90729a2b1e14e4e726e2
Ruan G. Molinari <110646641+ruan-molinari@users.noreply.github.com>
2023-10-31 10:50:55 -0300

    day4 optimization

--- src/day4.rs
+++ src/day4.rs
@@ -4,15 +4,20 @@ use md5::{Md5, Digest};
 
 #[aoc(day4, part1)]
 fn part1(input: &str) -> u32 {
-    solver(input, "00000")
+    solver(input, |hash| hash[0..2] == [0u8; 2] && (hash[2] & 0xF0) == 0)
 }
 
 #[aoc(day4, part2)]
 fn part2(input: &str) -> u32 {
-    solver(input, "000000")
+    solver(input, |hash| hash[0..3] == [0u8; 3])
 }
 
-fn solver(input: &str, str_start: &str) -> u32 {
+// this is my first attempt in solving the problem
+// where `str_start` was something like "00000".
+// this method was too slow though, so I took
+// a different approach based on gobanos's solution
+// (https://github.com/gobanos/advent-of-code-2015/blob/master/src/day4.rs)
+fn _solver(input: &str, str_start: &str) -> u32 {
     let mut i = 0;
     loop {
         let mut hasher = Md5::new();
@@ -24,6 +29,23 @@ fn solver(input: &str, str_start: &str) -> u32 {
         }
         i += 1;
     }
+}
+
+// this is the new implementation, it's ~50% faster,
+// although roughly half a second is needed still.
+// TODO: add concurrency to the loop
+fn solver(input: &str, is_valid: fn(&[u8; 16]) -> bool) -> u32 {
+    (1..=u32::MAX)
+        .map(|i| {
+            //let mut hasher = ;
+            //hasher.chain_update(&i.to_string());
+            let mut hasher = Md5::new_with_prefix(input);
+            hasher.update(&i.to_string());
+            let hash = Into::<[u8; 16]>::into(hasher.finalize());
+            (i, is_valid(&hash))
+        }).find(|&(_, h)| h)
+        .map(|(i, _)| i)
+        .expect("value overflowed u32")
 }
 
 #[cfg(test)]
